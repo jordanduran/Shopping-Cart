d6e0b404ad8cb061dd86def90cab95d36db40f0b
Jordan Duran <jordanduran@emv126.derooeinea.com>
2020-09-03 18:23:29 -0400

    useState, useRef import and shopping cart

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,16 +1,33 @@
-import React from 'react';
+import React, { useState, useRef } from 'react';
 import CartIcon from '../supermarket.svg';
+import useOnClickOutside from 'use-onclickoutside';
 
-export default function Header() {
+const Header = () => {
+  const modalRef = useRef(null);
+  const [isOpen, setIsOpen] = useState(false);
+
+  // Close modal if we click outside
+  useOnClickOutside(modalRef, () => setIsOpen(false));
+
   return (
     <header>
       <div className="container">
         <div className="cart-button">
-          <button>
-            <img src={CartIcon} width="30" />({0})
+          <button onClick={() => setIsOpen((isOpen) => !isOpen)}>
+            <img src={CartIcon} width="30" alt="cart" />({0})
           </button>
+          {/* Show Modal */}
+          <div
+            ref={modalRef}
+            className="cart-modal"
+            style={{ display: isOpen ? 'block' : 'none' }}
+          >
+            Cart goes here
+          </div>
         </div>
       </div>
     </header>
   );
-}
+};
+
+export default Header;
