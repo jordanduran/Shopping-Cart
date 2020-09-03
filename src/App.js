85ec9ac16ef7675af4d206b0b55834a575b4f49c
Jordan Duran <jordanduran@emv126.derooeinea.com>
2020-09-03 19:30:47 -0400

    CartProvider

--- src/App.js
+++ src/App.js
@@ -2,23 +2,26 @@ import React from 'react';
 import Header from './components/Header';
 import Product from './components/Product';
 import products from './products';
+import { CartProvider } from './contexts/use-cart';
 import './App.css';
 
 const App = () => {
   return (
-    <div className="app">
-      {/* header */}
-      <Header />
+    <CartProvider>
+      <div className="app">
+        {/* header */}
+        <Header />
 
-      <main>
-        <div className="products-list">
-          {/* show products here */}
-          {products.map((product, index) => (
-            <Product key={index} product={product} />
-          ))}
-        </div>
-      </main>
-    </div>
+        <main>
+          <div className="products-list">
+            {/* show products here */}
+            {products.map((product, index) => (
+              <Product key={index} product={product} />
+            ))}
+          </div>
+        </main>
+      </div>
+    </CartProvider>
   );
 };
 
