33078b728f5cbd9c81bed79b5a8bce9cb4072387
Jordan Duran <jordanduran@emv126.derooeinea.com>
2020-09-04 12:28:49 -0400

    Showing products in cart

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,13 +1,32 @@
 import React from 'react';
+import { useCart } from '../contexts/use-cart';
 
 const Cart = () => {
-  // const { addItem, removeItem, totalPrice, cart } = useCart();
+  const { addItem, removeItem, cartGroupedByItems, totalPrice } = useCart();
 
   return (
     <div className="cart">
       {/* show cart items here */}
+      {cartGroupedByItems.map((product, index) => (
+        <div className="cart-item" key={index}>
+          <img
+            src={product.image_url}
+            alt={product.name}
+            width="60"
+            height="60"
+          />
+          <div className="content">
+            <h3>{product.name}</h3>
+            <div className="cart-buttons">
+              <button onClick={() => removeItem(product.sku)}>-</button>
+              <button>{product.quantity}</button>
+              <button onClick={() => addItem(product.sku)}>+</button>
+            </div>
+          </div>
+        </div>
+      ))}
 
-      <div className="total">$100</div>
+      <div className="total">${totalPrice}</div>
     </div>
   );
 };
